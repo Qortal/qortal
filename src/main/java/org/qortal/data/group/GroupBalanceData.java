71d7d99f2622bec722dc9d1f4da0bfa43a868cd8
Qortal Seth <10013521+QortalSeth@users.noreply.github.com>
2026-07-22 14:44:42 -0600

    restored .gitignore.
    Updated groupFeeHeight feature trigger value

--- src/main/java/org/qortal/data/group/GroupBalanceData.java
+++ src/main/java/org/qortal/data/group/GroupBalanceData.java
@@ -9,15 +9,15 @@ public class GroupBalanceData extends GroupData{
     public GroupBalanceData() {
     }
 
-    public GroupBalanceData(Integer groupId, String owner, String groupName, String description, long created, Long updated, boolean isOpen, Group.ApprovalThreshold approvalThreshold, int minBlockDelay, int maxBlockDelay, byte[] reference, int creationGroupId, String reducedGroupName, int memberCount, long balance) {
-        super(groupId, owner, groupName, description, created, updated, isOpen, approvalThreshold, minBlockDelay, maxBlockDelay, reference, creationGroupId, reducedGroupName);
+    public GroupBalanceData(Integer groupId, String owner, String groupName, String description, long created, Long updated, boolean isOpen, Group.ApprovalThreshold approvalThreshold, int minBlockDelay, int maxBlockDelay, long joinFee, byte[] reference, int creationGroupId, String reducedGroupName, int memberCount, long balance) {
+        super(groupId, owner, groupName, description, created, updated, isOpen, approvalThreshold, minBlockDelay, maxBlockDelay, joinFee, reference, creationGroupId, reducedGroupName);
 
         this.memberCount = memberCount;
         this.balance = balance;
     }
 
-    public GroupBalanceData(String owner, String groupName, String description, long created, boolean isOpen, Group.ApprovalThreshold approvalThreshold, int minBlockDelay, int maxBlockDelay, byte[] reference, int creationGroupId, String reducedGroupName, int memberCount, long balance) {
-        super(owner, groupName, description, created, isOpen, approvalThreshold, minBlockDelay, maxBlockDelay, reference, creationGroupId, reducedGroupName);
+    public GroupBalanceData(String owner, String groupName, String description, long created, boolean isOpen, Group.ApprovalThreshold approvalThreshold, int minBlockDelay, int maxBlockDelay, long joinFee, byte[] reference, int creationGroupId, String reducedGroupName, int memberCount, long balance) {
+        super(owner, groupName, description, created, isOpen, approvalThreshold, minBlockDelay, maxBlockDelay, joinFee, reference, creationGroupId, reducedGroupName);
 
         this.memberCount = memberCount;
         this.balance = balance;
